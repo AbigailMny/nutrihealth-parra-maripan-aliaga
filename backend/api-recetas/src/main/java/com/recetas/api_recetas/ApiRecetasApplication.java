93b0925787c55c1c26b23af67a2475f898a35d75
AbigailMny <abigail.bao0@gmail.com>
2026-05-11 16:51:28 -0400

    Se añaden carpetas principales alimentacion

---
+++ backend/api-recetas/src/main/java/com/recetas/api_recetas/ApiRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.recetas.api_recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRecetasApplication.class, args);
+	}
+
+}
