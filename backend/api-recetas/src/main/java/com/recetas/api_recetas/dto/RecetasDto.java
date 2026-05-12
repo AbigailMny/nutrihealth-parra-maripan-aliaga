5fd54169dccd2351a7458d2ab90821776732a3a9
AbigailMny <abigail.bao0@gmail.com>
2026-05-12 11:59:51 -0400

    arreglo de errores

--- backend/api-recetas/src/main/java/com/recetas/api_recetas/dto/RecetasDto.java
+++
@@ -1,9 +0,0 @@
-package com.recetas.api_recetas.dto;
-import lombok.Data;
-
-@Data
-public class RecetasDto {
-    private Long id_receta;
-    private String nombre_plato;
-    private String preparacion;
-}
