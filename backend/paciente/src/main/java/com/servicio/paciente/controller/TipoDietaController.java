26dae23e6809a8b67e03661bcc5d63d0b6b6f49c
JavierPKS <javierparrareyes12345@gmail.com>
2026-05-12 23:36:37 -0400

    Se realizan cambio de errores en direccion y se añade datos_de prueba

--- backend/paciente/src/main/java/com/servicio/paciente/controller/TipoDietaController.java
+++ backend/paciente/src/main/java/com/servicio/paciente/controller/TipoDietaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tipos-dieta")
+@RequestMapping("/api/v1/tipos-dieta")
 public class TipoDietaController {
 
     @Autowired
